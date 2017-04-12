c01a5788c1a21419c83fa4e612794814755bd605
Eytan Avisror <Eytan_Avisror@intuit.com>
2017-04-13 00:15:54 +0300

    fixes to chef

--- cookbooks/mybb/recipes/default.rb
+++ cookbooks/mybb/recipes/default.rb
@@ -63,7 +63,7 @@ template "#{node['mybb']['deployment_path_inc']}/config.php" do
   action :create
 end
 
-file "#{node['mybb']['deployment_path']/lock" do
+file "#{node['mybb']['deployment_path']}/install/lock" do
   mode '0755'
   owner 'root'
   group 'root'
